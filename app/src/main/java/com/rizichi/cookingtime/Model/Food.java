1ae0f00aa7a6e002b7053c9336800cb499f6d0c7
amir <Amirr999999@gmail.com>
2023-09-24 17:39:04 +0330

    Food.java is added as Base Model of the Applivation

---
+++ app/src/main/java/com/rizichi/cookingtime/Model/Food.java
@@ -0,0 +1,13 @@
+package com.rizichi.cookingtime.Model;
+
+public class Food {
+    String name;
+    int pic;
+    int time;
+
+    public Food(String name, int pic, int time) {
+        this.name = name;
+        this.pic = pic;
+        this.time = time;
+    }
+}
